cb39ff02c9c3657d7466d0c4b419db1a4e9256de
Michael Truong <michaeltruong@Michaels-MacBook-Air.local>
2020-11-08 18:41:43 -0500

    edited main.js

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -1,14 +1,64 @@
-console.log("JavaScript file is running...");
-// Global currPlayer toggler
-let currPlayer = "X";
+/**
+ * Cached DOM Nodes
+ */ 
+const clickedCells = document.querySelectorAll(".cell");
 const gameStatus = document.querySelector(".game-status");
 gameStatus.innerText = `Please click a cell to start the game`;
+
+/**
+ * Global currPlayer Toggler
+ */
+let currPlayer = "X";
 const currentPlayerTurn = () => gameStatus.innerText = `It is now ${currPlayer}'s turn, please click a cell`;
 
-// Cached Array of All Queried <divs> 
-const clickedCells = document.querySelectorAll(".cell");
+/**
+ * Handle Cell Clicked Function
+ */
+const handleCellClicked = (cell) => {
+    if (cell.innerHTML === '' && currPlayer === "X") {
+        console.log("Handle X is executing");
+        handleXPlayer(cell);
+        switchPlayer();
+    } else if (cell.innerHTML === '' && currPlayer === "O") {
+        console.log("Handle O is executing");
+        handleOPlayer(cell);
+        switchPlayer();
+    }
+    winningConditions(clickedCells);
+}
+/**
+ * Handle "X" Player Helper Function
+ */
+const handleXPlayer = (cell) => {
+    cell.innerText = "X";
+    cell.classList.add("optionX");
+    cell.classList.remove("optionO");
+}
+/**
+ * Handle "O" Player Helper Function
+ */
+const handleOPlayer = (cell) => {
+    cell.innerText = "O";
+    cell.classList.add("optionO");
+    cell.classList.remove("optionX");
+}
 
-const switchCaseStatements = (cells) => {
+/**
+ * Switch Player from letter "X" or "O"
+ */
+const switchPlayer = () => {
+    if (currPlayer === "X") {
+        currPlayer = "O";
+    } else if (currPlayer === "O") {
+        currPlayer = "X";
+    }
+    gameStatus.innerHTML = currentPlayerTurn();
+}
+
+/**
+ * If-Else Win Conditions
+ */
+const winningConditions = (cells) => {
     if (cells[0].innerHTML === "X" && cells[1].innerHTML === "X" && cells[2].innerHTML === "X") {
         alert("Player X wins!");
         location.reload();
@@ -52,55 +102,9 @@ const switchCaseStatements = (cells) => {
         alert("Player O wins!");
         location.reload();
     } else if (cells[0].innerHTML !== '' && cells[1].innerHTML !== '' && cells[2].innerHTML !== '' && cells[3].innerHTML !== '' && cells[4].innerHTML !== '' && cells[5].innerHTML !== '' && cells[6].innerHTML !== '' && cells[7].innerHTML !== '' && cells[8].innerHTML !== '') {
-        alert("It's a cat's tie!")
+        alert("It's a Cat's Game!")
         location.reload();
     }
-}
-
-/**
- * SwitchPlayer from letter "X" or "O"
- */
-const switchPlayer = () => {
-    if (currPlayer === "X") {
-        currPlayer = "O";
-    } else if (currPlayer === "O") {
-        currPlayer = "X";
-    }
-    gameStatus.innerHTML = currentPlayerTurn();
-}
-
-/**
- * Handle cell clicked function
- */
-const handleCellClicked = (cell) => {
-    if (cell.innerHTML === '' && currPlayer === "X") {
-        console.log("Handle X is executing");
-        handleXPlayer(cell);
-        // switchCaseStatements(clickedCells);
-        switchPlayer();
-    } else if (cell.innerHTML === '' && currPlayer === "O") {
-        console.log("Handle O is executing");
-        handleOPlayer(cell);
-        
-        switchPlayer();
-    }
-    switchCaseStatements(clickedCells);
-}
-/**
- * Handle "X" Player
- */
-const handleXPlayer = (cell) => {
-    cell.innerText = "X";
-    cell.classList.add("optionX");
-    cell.classList.remove("optionO");
-}
-/**
- * Handle "O" Player
- */
-const handleOPlayer = (cell) => {
-    cell.innerText = "O";
-    cell.classList.add("optionO");
-    cell.classList.remove("optionX");
 }
 
 // loop through all cell-nodes in querySeletorAll Array and addEventListener 
@@ -114,12 +118,10 @@ const toggleXO = (cells) => {
 
 // * Reset Button * //
 const resetBtn = document.querySelector("#reset");
-// Adds a Reset Button EventListener with resetAllCells() callback
 resetBtn.addEventListener("click", () => {
     clickedCells.forEach(cell => {
         cell.innerText = '';
     });
-    location.reload();
 });
 
 // =========== START OF ADD-EVENT-LISTENER INVOKATIONS ========== // 
